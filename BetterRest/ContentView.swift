17b9ce115897ef31b3eee9e394e45052cfe14870
wreckss <46939881+Wreckss@users.noreply.github.com>
2020-07-28 21:16:41 -0600

    Finished calculateBedtime
    Ideal bedtime is calculated using the CoreML model in SleepCalculator(). Afterwards, result is formatted and displayed within an Alert.

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -12,6 +12,10 @@ struct ContentView: View {
     @State private var wakeUpTime = Date()
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
   
     var body: some View {
         NavigationView {
@@ -42,17 +46,46 @@ struct ContentView: View {
                 }
             }
         .navigationBarTitle("BetterRest")
-            navigationBarItems(trailing:
+            .navigationBarItems(trailing:
                 Button(action: calculateBedtime) {
                     Text("Calculate")
             })
+                .alert(isPresented: $showingAlert) {
+                    Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+            }
         }
     }
     
     func calculateBedtime() {
         let model = SleepCalculator()
         
+        //this provides all the details of the current date
+        let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUpTime)
         
+        /*Datecomponents returns values formatted as seconds,
+        multiply those seconds by 60 to get minutes,
+        multiply those minutes by 60 to get hour*/
+        let hour = (components.hour ?? 0) * 60 * 60
+        let minute = (components.minute ?? 0) * 60
+        
+        do {
+            let prediction = try
+            model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUpTime - prediction.actualSleep
+            
+            //this converts the sleepTime Date var into a readable string
+            let formatter = DateFormatter()
+            formatter.timeStyle = .short
+            
+            alertMessage = formatter.string(from: sleepTime)
+            alertTitle = "Your ideal bedtime is..."
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
+        
+        showingAlert = true
     }
 }
 
